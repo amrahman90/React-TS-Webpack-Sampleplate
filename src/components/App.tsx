42f8fcb5c010e6609767856913cc8437b0865f77
AM Rahman <almahmudur.rahman@qundis.com>
2021-03-08 11:53:46 +0100

    tsconfig updated and React FunctionComponent added

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <h1> My React and TypeScript App!! {new Date().toLocaleDateString()}</h1>
+  )
+};
+
+export default App;
